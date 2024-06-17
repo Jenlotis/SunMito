b84aaa014234655c4e4a8422cb8f0f05a9e53959
Jenlotis <101654347+Jenlotis@users.noreply.github.com>
2024-06-17 13:58:35 +0200

    added small install script
    installs all necessary programs and libraries for the program to work, possible that it is temporary

---
+++ instal_me.sh
@@ -0,0 +1,12 @@
+sudo apt-get install --assume-yes python-pip python3 python3-pip
+pip install dash
+pip install numpy
+pip install pandas
+pip install plotly
+pip install multiqc
+sudo apt-get install --assume-yes fastqc
+sudo apt-get install --assume-yes conda
+conda install -c bioconda install chopper
+sudo apt-get install --assume-yes trimmomatic
+sudo apt-get install --assume-yes r-base
+wget -P programs https://raw.githubusercontent.com/roblanf/minion_qc/master/MinIONQC.R
