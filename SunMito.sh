a4ec2c17b9c458d669a24f92900456f788bb86a4
Jenlotis <101654347+Jenlotis@users.noreply.github.com>
2024-07-08 22:00:08 +0200

    Add files via upload
    created a way to install and run mitofinder on every linux distro

--- SunMito.sh
+++ SunMito.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
 sudo apt-get install --assume-yes python-pip python3 python3-pip
-#curl https://pyenv.run | bash
-#echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
-#echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
-#echo 'eval "$(pyenv init -)"' >> ~/.bashrc
-#exec "$SHELL"
-#pyenv install 2.7.18
 pip install dash
 pip install numpy
 pip install pandas
 pip install plotly
 pip install multiqc
+pip install conda
 sudo apt-get install automake autoconf
 sudo apt install default-jre
 wget -P programs https://raw.githubusercontent.com/chrishah/MITObim/master/misc_scripts/downsample.py
@@ -18,6 +13,8 @@ wget -P programs/novo https://github.com/Edith1715/NOVOplasty/archive/refs/heads
 unzip programs/novo/master.zip -d programs/ 
 wget -P programs/mitfi https://github.com/RemiAllio/MitoFinder/archive/master.zip
 unzip programs/master.zip -d programs/ 
+conda create --name MitFi mitofinder -c bioconda
+mv programs/MitoFinder-master/install.sh programs/MitoFinder-master/install.sh.ok
 sudo apt-get install --assume-yes bbmap
 sudo apt-get install --assume-yes fastqc
 sudo apt-get install --assume-yes conda
