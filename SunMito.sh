2f9ed50aad286ef1ba1d4861e3f434dc135f1fda
Jenlotis <101654347+Jenlotis@users.noreply.github.com>
2024-09-01 17:16:20 +0200

    Add files via upload
    changed the way conda is installed

--- SunMito.sh
+++ SunMito.sh
@@ -4,11 +4,11 @@ python3 -m venv sunmito
 source sunmito/bin/activate
 pip install dash
 pip install dash-bootstrap-components
+pip install dash-daq
 pip install numpy
 pip install pandas
 pip install plotly
 pip install multiqc
-pip install conda
 sudo apt-get install automake autoconf
 sudo apt install default-jre
 wget -P programs https://raw.githubusercontent.com/chrishah/MITObim/master/misc_scripts/downsample.py
@@ -20,7 +20,12 @@ conda create --name MitFi mitofinder -c bioconda
 mv programs/MitoFinder-master/install.sh programs/MitoFinder-master/install.sh.ok
 sudo apt-get install --assume-yes bbmap
 sudo apt-get install --assume-yes fastqc
-sudo apt-get install --assume-yes conda
+mkdir -p ~/miniconda3
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
+bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
+rm ~/miniconda3/miniconda.sh
+~/miniconda3/bin/conda init bash
+~/miniconda3/bin/conda init zsh
 conda install -c bioconda seqkit
 conda install -c bioconda chopper
 sudo apt-get install --assume-yes trimmomatic
